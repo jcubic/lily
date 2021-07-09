68f97e7b5c16ff6ca7bcdc3156c6f3c180953096
Jakub Jankiewicz <jcubic@onet.pl>
2021-07-09 16:46:19 +0200

    fix parser + add unit tests

---
+++ __test__/lily.spec.js
@@ -0,0 +1,58 @@
+const lily = require('..');
+
+
+test('should parse short options', () => {
+  expect(lily(['-a', '-b'])).toStrictEqual({_: [], a: true, b: true});
+});
+
+test('should parse short combined options', () => {
+  expect(lily(['-abc', '-e'])).toStrictEqual({_: [], a: true, b: true, c: true, e: true});
+});
+
+test('should parse long option', () => {
+  expect(lily(['--foo', 'hello'])).toStrictEqual({_: [], foo: 'hello'});
+});
+
+test('should parse two long successive option', () => {
+  expect(lily(['--foo', '--hello'])).toStrictEqual({_: [], foo: true, hello: true});
+});
+
+test('should parse two long successive option and argument', () => {
+  expect(lily(['--foo', '--hello', 'world'])).toStrictEqual({_: [], foo: true, hello: 'world'});
+});
+
+test('should parse long options as boolean', () => {
+  expect(lily(['--hello', '-abc'])).toStrictEqual({_: [], a: true, b: true, c: true, hello: true});
+});
+
+test('should parse short combined before long option', () => {
+  expect(lily(['-abc', '--hello'])).toStrictEqual({_: [], a: true, b: true, c: true, hello: true});
+});
+
+test('should parse short option with argument', () => {
+  expect(lily(['-abc', 'foo', '--hello'])).toStrictEqual({_: [], a: true, b: true, c: "foo", hello: true});
+});
+
+test('should parse long option with argument', () => {
+  expect(lily(['-abc', 'foo', '--hello', 'bar'])).toStrictEqual({_: [], a: true, b: true, c: "foo", hello: 'bar'});
+});
+
+test('should parse long boolean option', () => {
+  expect(lily(['--hello', '--world'])).toStrictEqual({_: [], hello: true, world: true});
+});
+
+test('should parse rest arguments after short option', () => {
+  expect(lily(['-abc', 'hello', 'world'])).toStrictEqual({_: ['world'], a: true, b: true, c: 'hello'});
+});
+
+test('should parse rest arguments after long option', () => {
+  expect(lily(['--foo', 'hello', 'world'])).toStrictEqual({_: ['world'], foo: 'hello'});
+});
+
+test('should handle short boolean options', () => {
+  expect(lily(['-abc', 'hello', 'world'], { boolean: ['c']})).toStrictEqual({_: ['hello', 'world'], a: true, b: true, c: true});
+});
+
+test('should handle long boolean options', () => {
+  expect(lily(['--foo', 'hello', 'world'], { boolean: ['foo']})).toStrictEqual({_: ['hello', 'world'], foo: true});
+});
--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+/**
+ * LILY is very simple option parser inspired by YARGS with no dependencies
+ *
+ * Copyright (C) 2020 [Jakub T. Jankiewicz](https://jcubic.pl)
+ *
+ * Released under MIT license
+ */
 module.exports = function parse_options(arg, options) {
     var settings = Object.assign({}, {
         boolean: []
@@ -12,18 +19,25 @@ module.exports = function parse_options(arg, options) {
         if (typeof arg !== 'string') {
             arg = String(arg);
         }
-        if (arg.match(/^-/) && acc instanceof token) {
+        // last short option followed by option e.g. --abc --foo
+        if (acc instanceof token && acc.value.length === 1 && arg.match(/^-/)) {
+            result[acc.value] = true;
+        } else if (arg.match(/^-[^-]/) && acc instanceof token) {
             result[acc.value] = true;
         }
-        if (arg.match(/^--/)) {
+        if (arg.match(/^--./)) {
+            if (acc instanceof token) {
+                result[acc.value] = true;
+            }
             var name = arg.replace(/^--/, '');
             if (settings.boolean.indexOf(name) === -1) {
                 return new token(name);
             } else {
                 result[name] = true;
             }
-        } else if (arg.match(/^-/)) {
+        } else if (arg.match(/^-[^-]/)) {
             var single = arg.replace(/^-/, '').split('');
+            // handling short option that have argument -abc foo c=foo
             if (settings.boolean.indexOf(single.slice(-1)[0]) === -1) {
                 var last = single.pop();
             }
